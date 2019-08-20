06ef9d0ae5ae25debc1569cb7d11c0110290bf65
EmmanuelD69 <emmanueldurouge@yahoo.com>
2019-08-20 12:30:13 +0200

    Update main.js

--- Carnet de contacts/js/main.js
+++ Carnet de contacts/js/main.js
@@ -6,6 +6,7 @@ function onClicshowForm(){
     // $("form").toggleClass('hide')
     // $("form").toggle()
     $("form").fadeToggle()
+    $("form").data("mode", "add") /* passe l'attribut "mode" du formulaire en mode "add" (ajout) */
 }
 
 /* Cette fonction permet de consulter le cache local du nagivateur web, et le cas échant, de retourner les infos qui y sont stockées. */
@@ -22,12 +23,28 @@ function checkLocalStorage(){
 function onClickSaveContact(event){
     event.preventDefault() /* Interruption de l'action submit du formulaire, afin de pouvoir executer les instructions de ma fonction */
     checkLocalStorage() /* récupération des infos JSON stockées en local, s'il y'en a */
-    var contact = new Object()  /* création d'un objet qui va récupérer les infos du formulaire */ 
+
+    var mode = $("#formulaire").data("mode")
+    var index= $("#infosContact a").data('index')
+
+    var contact = new Object()  /* création d'un objet qui va récupérer les infos du formulaire */
     contact.civilite = $("#civilite").val() /* récupération depuis le formulaire et stockage de la civilité du contact dans l'objet "contact" */
     contact.nom = $("#nom").val()  /* récupération depuis le formulaire et stockage du Nom du contact dans l'objet "contact" */
     contact.prenom = $("#prenom").val() /* récupération depuis le formulaire et stockage du Prénom du contact dans l'objet "contact" */ 
     contact.telephone = $("#telephone").val() /* récupération depuis le formulaire et stockage du téléphone du contact dans l'objet "contact" */
-    liste.push(contact) /* envoi des infos contenus dans l'objet "contact" vers le tableau "liste" */
+
+    
+    if (mode == "edit") {
+        checkLocalStorage()
+        liste[index].civilite = $("#civilite").val()
+        liste[index].nom = $("#nom").val()
+        liste[index].prenom = $("#prenom").val()
+        liste[index].telephone = $("#telephone").val()
+    } 
+    
+    else {
+        liste.push(contact) /* envoi des infos contenus dans l'objet "contact" vers le tableau "liste" */
+    }    
     $("#formulaire")[0].reset() /* Reset du formulaire */
     var JqueryToJason = JSON.stringify(liste, null, '\t') /* conversion des infos contenus dans le tableau "liste" en données JSON */ 
     localStorage.setItem("carnet", JqueryToJason) /* Enregistrement des données JSON en cache local sur le navigateur */
@@ -39,7 +56,7 @@ function onClickSaveContact(event){
 /* Cette fonction sert à effacer complètement la liste de contact, y compris dans le cache local */
 function onClickClearAddressBook(){
     $("#contactList").empty() /* Effacement de l'ancienne liste de contacts affichée sur la page web */
-    $("#infosContact h3").empty() /* Effacement des infos sur le contact affichée sur la page web */
+    $("#infosContact h3, #infosContact p").empty() /* Effacement des infos sur le contact affichée sur la page web */
     localStorage.clear() /* Effacement de l'ensemble des données contenues dans le cache local du navigateur */
 }
 
@@ -48,34 +65,45 @@ function listCarnetContact(){
     checkLocalStorage() /* appel à la fonction "checkLocalStorage" */
     $('#contactList').html('<ul>') /* Création d'une ul pour y insérer les li correspondant à nos contacts enregistrés */
     for(i=0; i < liste.length; i++){ /* pour chaque objets contenus dans le tableau "liste" */
-        $("#contactList ul").append("<li>" + '<a href="#" data-index="' +i+ '">' + liste[i].prenom + " " + liste[i].nom + "</a>" + "</li>") /* Affiche le prénom suivi du nom dans une li et attribut à chaque li un index qui sera utilisé pour afficher le détail du contact lorsqu'on clic sur son nom.*/
+        $("#contactList ul").append("<li>" + '<a href="#" data-index="' +i+ '">'+ liste[i].prenom + " " + liste[i].nom + "</a>" + "</li>") /* Affiche le prénom suivi du nom dans une li et attribut à chaque li un index qui sera utilisé pour afficher le détail du contact lorsqu'on clic sur son nom.*/
     }
 }
 
 function displayContact(){
-    //récupérer le data-index
-    let civilite
+    if(infosContact.style.display == "none"){
+        $("#infosContact").toggle()        
+    }    
     var index = $(this).attr('data-index') /* Récupère l'index du contact sur lequel on a clic */
-    console.log(index)
-    //récupère les infos du contact    
-    switch (index) {
-        case "0":
-            civilite = "Madame" 
-            $("#infosContact h3").text(civilite + " " + liste[index].prenom + " " + liste[index].nom)
-            break;
+    switch (liste[index].civilite) {
         case "1":
-            civilite = "Mademoiselle" 
-            $("#infosContact h3").text(civilite + " " + liste[index].prenom + " " + liste[index].nom)
+            civilite = "Madame" 
+            $("#infosContact h3").text("Madame " + liste[index].prenom + " " + liste[index].nom)
             break;
         case "2":
+            civilite = "Mademoiselle" 
+            $("#infosContact h3").text("Mademoiselle " + liste[index].prenom + " " + liste[index].nom)
+            break;
+        case "3":
             civilite = "Monsieur" 
-            $("#infosContact h3").text(civilite + " " + liste[index].prenom + " " + liste[index].nom)
+            $("#infosContact h3").text("Monsieur " + liste[index].prenom + " " + liste[index].nom)
             break;
     }
     $("#infosContact p").text(liste[index].telephone)
+    $("#infosContact a").data('index', index)
 }
 
-
+function editContact(){
+    var index = $(this).data('index') /* Récupère l'index du contact sur lequel on a clic */
+    /* affiche le formulaire et indique qu'on est en mode "edit" */
+    $("#formulaire").data("mode", "edit").fadeIn()
+    /* Récupère les infos des contacts enregistrés en cache local */
+    checkLocalStorage()
+    /* afficher la valeur de chaque champs pour le contact qu'on edit */
+    $("#civilite").val(liste[index].civilite)
+    $("#nom").val(liste[index].nom)
+    $("#prenom").val(liste[index].prenom)
+    $("#telephone").val(liste[index].telephone)
+}
 
 /* code qui ne s'éxécute qu'une fois que la page HTML est bien chargée */
 $(document).ready(function(){
@@ -83,7 +111,9 @@ $(document).ready(function(){
     $("#btn").on('click', onClickSaveContact) /* mise en place d'un écouteur d'évènement */
     $("#effacer").on('click', onClickClearAddressBook) /* mise en place d'un écouteur d'évènement */
     $("form").toggle() /* On cache le formulaire */
+    $("#infosContact").toggle() /* On cache les détails des contacts */
     $("#contactList").on('click', 'ul li a', displayContact) /* mise en place d'un écouter d'évènement qui se déclenchera lorsqu'on va clic sur le nom d'un contact */
+    $("#infosContact a").on('click', editContact)
     checkLocalStorage() /* appel à la fonction "checkLocalStorage" afin de verifier s'il exite des données dans le cache local au moment du chargement de la page */
     listCarnetContact() /* appel à la fonction "listCarnetContact" afin d'afficher la liste de contact au moment du chargement de la page */
     })
